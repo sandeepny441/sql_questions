591507918cecdfc0acb03bd06794fd90ae8270ef
Sandeep Reddy <sandeepny441@gmail.com>
2023-11-11 13:50:47 -0600

    added SQL folders

---
+++ SQL_Questions_Answers/e3_string_functions.py
@@ -0,0 +1,13 @@
+-- select upper('apple') --APPLE
+-- select lower('APPLE') --apple
+-- select SUBSTRING('Apple', 3) --ple
+-- select SUBSTRING('ORANGE', 2, 4) --RANG
+-- select length('SPACE') --3
+-- select concat('APPLE', '-- ORANGE') -- APPLE-- ORANGE
+-- SELECT LTRIM(' hello') --hello
+-- SELECT LTRIM('hello ') --hello
+-- select trim(' HELLO ') --HELLO
+-- select replace('HELLO', 'HE', 'SHE') --SHELLO
+-- select reverse('APPLE') -- ELPPA
+-- select CHAR_LENGTH('APPLE') --5
+-- select locate('d', 'APPLE world') --11
