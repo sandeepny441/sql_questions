6060da0acf53c1d8056df74e1895d91b0dc9bbd2
Sandeep Reddy <sandeepny441@gmail.com>
2023-11-11 15:06:18 -0600

    added SQL folders

--- SQL_Questions_Answers/a_basics.py
+++ SQL_Questions_Answers/a_basics.py
@@ -21,18 +21,22 @@ SELECT
 
 ==============================================================================
 
-* 
+START  * 
 # How would you fetch all columns and all rows from the Trips table?
 # select * from trips
+
 # If you wanted to view the entire record for trips with a duration of 7 days, how would you write the query?
 # select * from trips
 # where DurationDays = 7
+
 # How can you see all details for trips that have 'Beach' in their TripName?
 # select * from trips
 # where TripName LIKE "%Beach%"
+
 # Write a query that shows every column for trips that cost more than $1500.
 # select * from trips
 # where Price > 1500
+
 # How would you retrieve the full details of the cheapest trip in the Trips table?
 # select*from trips 
 # where Price IN ( select min(Price) from trips)
@@ -44,15 +48,22 @@ COUNT
 
 # What query would give you the total number of trips offered in the Trips table?
 # select count(*) from trips 
+
 # How would you determine the number of trips that have a duration longer than 5 days?
 # select count(*) from trips 
 # where DurationDays > 5
+
+
 # How can you count the number of trips with a destination of 'Maldives'?
 # select count(*) from trips 
 # where Destination = 'Maldives'
+
+
 # Can you find out how many trips cost less than $1000?
 # select count(*) from trips 
 # where Price < 1000
+
+
 # What would the query be to count the number of trips that have 'Adventure' in their TripName?
 # select count(*) from trips 
 # where TripName LIKE '%adventure%'
@@ -62,21 +73,39 @@ DISTINCT
 
 # How would you retrieve a list of unique destinations from the Trips table?
 # select DISTINCT(TripName) from trips
+
 # Can you get a list of unique DurationDays values from the table?
 # select DISTINCT(DurationDays) from trips
+
 # How do you find out the distinct price points for the trips?
 # select DISTINCT(Price) from trips
+
 # If you wanted to see the different words used in the TripName column (assuming each trip name contains only one word), how would you fetch them?
 # select DISTINCT(TripName) from trips
+
 # What query would you write to see all the unique combinations of Destination and DurationDays?
 # select DISTINCT Destination, DurationDays from trips
 
 =====================================================================
 AND 
 # How would you fetch trips that are priced between $500 and $1500?
+select TripName 
+from trips 
+where Price between 500 and 1500
+
+
 # Can you retrieve all trips with a destination of 'Maldives' that have a duration of 5 days?
+select * 
+from trips 
+where Destination In ("Maldives")
+and DurationDays < 7 
+
 # Write a query to get the TripName and Price of trips that are priced above $1000 and have a duration less than 7 days.
+
+
 # How do you select trips where the TripName contains the word "Beach" and the price is less than $1300?
+
+
 # Can you find trips where the destination is 'New York' and the duration is exactly 3 days?
 =====================================================================
 OR 
