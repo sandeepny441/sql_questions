99577b62e302320969d3969238a32aa795ba26b6
Sandeep Reddy <sandeepny441@gmail.com>
2023-11-15 18:18:46 -0600

    added topics

---
+++ 100_Qs/007_self_join.py
@@ -0,0 +1,63 @@
+# select * from T
+
+
+# Question 1:
+# Who are the managers of each employee? (Assuming that ManagerID corresponds to EmployeeID in the same table)
+
+
+# Question 2:
+# Which employees are peers within the same department? (Employees who share the same DepartmentID)
+
+# Question 3:
+# Find the names of all employees who do not manage anyone. (Employees whose EmployeeIDs do not appear in anyone else's ManagerID column)
+
+# Question 4:
+# Which managers have more than one employee reporting to them?
+
+# Question 5:
+# List all employees along with the names of their managers. (Assuming that managers are also listed as employees in the same table)
+
+# Question 6:
+# Are there any departments where the manager is also listed as an employee within the same department? If so, list those departments and manager names.
+
+# Question 7:
+# Find the employee names along with the names of their immediate subordinates.
+
+# Question 8:
+# Determine if there are any employees who share a manager but are from different departments.
+
+# Question 9:
+# Identify employees who are also managers of other departments (i.e., their EmployeeID appears in the ManagerID column for a different DepartmentID).
+
+# Question 10:
+# Identify any managers who report to other managers within the organization (assuming a hierarchy exists where some managers may also have managers).
+
+# Question 11:
+# List all employees who do not report to a manager within their own department (indicating a cross-department management structure).
+
+# Question 12:
+# For each department, find the employee with the longest name.
+
+# Question 13:
+# Can you identify any indirect reporting structures? For instance, an employee who reports to a manager who in turn reports to another manager.
+
+# Question 14:
+# Are there any employees who have the same name as their manager?
+
+# Question 15:
+# Which departments have a hierarchy depth greater than one, indicating that there are multiple levels of management within the department?
+
+# Question 16:
+# Which employees are managed by someone with a higher employee ID number?
+
+# Question 17:
+# Find the names of managers who are managing more than one department.
+
+# Question 18:
+# List all employees who have a manager in the same department and their manager has a manager in a different department.
+
+# Question 19:
+# Identify any managers who also act as an employee under another manager in a different department.
+
+# Question 20:
+# Determine if there are any employees who share the same manager and have the same name (excluding the manager themselves).
--- 100_Qs/008_top_1_2_percent.py
+++ 100_Qs/008_top_1_2_percent.py
@@ -1,7 +1,8 @@
-# Top Percentage (Adjusted for Small Dataset):
+# What is the most expensive product, as an equivalent to asking for the top 1% in terms of price?
 
-# Who is the top-ranked employee if we consider the top 20% in terms of EmployeeID order?
-# Top Percentage (Hypothetical for Larger Dataset):
+# What are the top two most expensive products in each category, simulating a top 4% query if the dataset were larger?
 
-# Can you list the employees who fall into the top 2% in terms of performance scores? (Assuming there is a performance score column)
-# Who are the employees in the top 4% in terms of seniority? (Assuming there is a column indicating the length of service)
+
+# Among the products listed, which belong to the most expensive 50%?
+
+# With the dataset provided, these questions would effectively translate to simply finding the highest prices or the median price point. In a larger dataset, you could use SQL window functions like NTILE() to divide the dataset into percentiles and then select the top Nth percentile accordingly.   
