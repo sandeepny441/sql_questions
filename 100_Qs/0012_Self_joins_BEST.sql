936486f2c39c447d777d4334be016431f7f3ed06
Sandeep Reddy <sandeepny441@gmail.com>
2023-11-16 16:35:13 -0600

    added topics

---
+++ 100_Qs/0012_Self_joins_BEST.sql
@@ -0,0 +1,103 @@
+-- Fetch Employees and Their Managers
+SELECT 
+    E1.EmployeeName AS Employee,
+    E2.EmployeeName AS Manager
+FROM 
+    Employee E1
+JOIN 
+    Employee E2 ON E1.ManagerID = E2.EmployeeID;
+-----------------------------------------------------
+-- EMployee Salary > Manager Salary 
+SELECT 
+    E1.EmployeeName AS Employee,
+    E1.Salary AS EmployeeSalary,
+    E2.EmployeeName AS Manager,
+    E2.Salary AS ManagerSalary
+FROM 
+    Employee E1
+JOIN 
+    Employee E2 ON E1.ManagerID = E2.EmployeeID
+WHERE 
+    E1.Salary > E2.Salary;
+-----------------------------------------------------
+SELECT 
+    E1.EmployeeName
+FROM 
+    Employee E1
+WHERE 
+    E1.ManagerID IS NULL;
+-----------------------------------------------------
+SELECT 
+    E1.EmployeeName AS Employee1,
+    E2.EmployeeName AS Employee2,
+    E1.ManagerID
+FROM 
+    Employee E1
+JOIN 
+    Employee E2 ON E1.ManagerID = E2.ManagerID
+WHERE 
+    E1.EmployeeID != E2.EmployeeID;
+-----------------------------------------------------
+
+SELECT 
+    E1.EmployeeName AS Employee1,
+    E2.EmployeeName AS Employee2,
+    E1.Salary
+FROM 
+    Employee E1
+JOIN 
+    Employee E2 ON E1.Salary = E2.Salary
+WHERE 
+    E1.EmployeeID != E2.EmployeeID;
+------------------------------------------------------
+SELECT 
+    E1.EmployeeName AS Manager,
+    E2.EmployeeName AS Subordinate
+FROM 
+    Employee E1
+JOIN 
+    Employee E2 ON E1.EmployeeID = E2.ManagerID;
+------------------------------------------------------------- 
+SELECT 
+    E1.EmployeeName AS Employee,
+    E2.EmployeeName AS TopManager
+FROM 
+    Employee E1
+JOIN 
+    Employee E2 ON E1.ManagerID = E2.EmployeeID
+WHERE 
+    E2.ManagerID IS NULL;
+------------------------------------------------------------
+SELECT 
+    E1.EmployeeName AS Manager
+FROM 
+    Employee E1
+WHERE 
+    E1.EmployeeID NOT IN (SELECT ManagerID FROM Employee WHERE ManagerID IS NOT NULL);
+--------------------------------------------------------------
+SELECT 
+    E1.EmployeeName,
+    E1.Salary
+FROM 
+    Employee E1
+WHERE 
+    E1.Salary > (SELECT AVG(Salary) FROM Employee);
+--------------------------------------------------------------
+SELECT 
+    E1.EmployeeName AS Employee1,
+    E2.EmployeeName AS Employee2,
+    E1.Role
+FROM 
+    Employee E1
+JOIN 
+    Employee E2 ON E1.Role = E2.Role
+WHERE 
+    E1.EmployeeID != E2.EmployeeID;
+--------------------------------------------------------------
+select 
+E1.empname as name, 
+E1.salary,
+E1.departmentID
+from Employee E1
+where E1.slary =  (select min(salary) from Employee E2 where E1.departmentID = E2.departmentID)
+--------------------------------------------------------------
--- 100_Qs/001_case_sum_25.py
+++ 100_Qs/001_case_sum_25.py
@@ -36,7 +36,39 @@
 # WHERE Destination IN ("Maldives", "Himalayas")
 # group by TripName
 ================================================================================================================
+# How would you use a CASE statement within the SUM function to calculate the total ticket sales for flights to London and Paris separately from the other destinations?
+# select
+# SUM(CASE WHEN Destination in ('London', 'Paris') THEN TicketPrice else 0 END) as total_price_PL, 
+# SUM(CASE WHEN Destination NOT in ('London', 'Paris') THEN TicketPrice else 0 END) as total_price_not_PL 
+# from navan 
+# Can you write a query that uses CASE within the AVG function to determine the average ticket price for domestic flights (assuming flights within the same country are considered domestic) and international flights?
+#  select AVG( CASE WHEN Destination ='London'THEN TicketPrice ELSE 0 END ) as L_Avg, AVG( CASE WHEN Destination ='Paris'THEN TicketPrice ELSE 0 END ) as P_avg from navan
 
+# How would you use CASE and COUNT to count the number of bookings with a ticket price greater than $600, and another count for those with a ticket price of $600 or less?
+
+
+
+
+# Can you create a query using CASE within the MIN and MAX functions to find the minimum and maximum ticket prices for flights departing before a certain date compared to those departing after that date?
+
+
+# How would you write a query to sum the ticket prices for each destination, using CASE to apply a 10% discount to flights to Rome?
+
+
+# Could you use CASE in conjunction with COUNT to differentiate between the number of flights to European destinations versus other destinations, given a list of European countries?
+
+
+
+# Can you demonstrate how to use CASE with the SUM function to total ticket prices, categorizing them into price ranges (e.g., <$500, $500-$600, >$600)?
+# How would you employ a CASE statement within AVG to find the average ticket price for flights on weekdays versus weekends?
+
+
+
+# Can you use CASE and MIN to determine the earliest flight date for flights with a ticket price below the average price and flights with a ticket price above the average price?
+
+
+
+# Could you write a query that uses CASE within the SUM function to calculate the total revenue from flights by quarter, assuming the fiscal year starts in April?
 ================================================================================================================
 
 
