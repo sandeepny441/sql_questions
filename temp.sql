37bb4d71835784b8377c7354bbef807b4bdcf9eb
Sandeep Reddy <sandeepny441@gmail.com>
2023-12-28 15:49:13 -0600

    added topics

---
+++ temp.sql
@@ -0,0 +1,17 @@
+The company you are working for wants to anticipate their staffing needs by identifying their top two busiest times of the week. To find this, each day should be segmented into differents parts using following criteria:
+
+
+
+Morning: Before 12 a.m. (not inclusive)
+
+Early afternoon: 12 -15 p.m.
+
+Late afternoon: after 15 p.m. (not inclusive)
+
+
+Your output should include the day and time of day combination for the two busiest times, i.e. the combinations with the most orders, along with the number of orders (e.g. top two results could be Friday Late afternoon with 12 orders and Sunday Morning with 10 orders). The company has also requested that the day be displayed in text format (i.e. Monday).
+
+
+Note: In the event of a tie in ranking, all results should be displayed.
+
+
