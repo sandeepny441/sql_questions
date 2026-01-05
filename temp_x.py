702e71ab9189468a0ebc7189a06c5b8be672b366
sandeepny441 <62777490+sandeepny441@users.noreply.github.com>
2026-01-05 16:14:32 -0500

    Update temp_x.py

--- temp_x.py
+++ temp_x.py
@@ -1,1 +1,10 @@
 
+df["date"] = pd.to_datetime(df["date"])
+df["month"] = df["date"].dt.to_period("M")
+
+result = (
+    df
+    .groupby(["nmls", "month", "loan_purpose"])
+    .size()
+    .reset_index(name="loan_count")
+)
